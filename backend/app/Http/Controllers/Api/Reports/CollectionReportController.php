73a1ca88faa2268ce3deb8b973372bf2146587d9
Ember Web Solutions Inc <emberwebsolutions2025@gmail.com>
2026-06-22 21:04:04 +0800

    fix: restore top delinquent accounts route

---
+++ backend/app/Http/Controllers/Api/Reports/CollectionReportController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\Reports;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CollectionReportController extends Controller
+{
+    //
+}
